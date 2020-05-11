8c87a83373095d0536267785f527daa7c4ac7ec0
constantinmaes <constantinmaes@gmail.com>
2020-05-11 08:57:53 +0200

    :sparkles: feat(orm): scaffold app for typeorm usage

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import { Connection, createConnection} from 'typeorm';
 import { Routes } from './config/routes';
 
 class App {
   public app: express.Application;
+  public connection: Connection;
   public routePrv: Routes = new Routes();
 
   constructor () {
-    this.app =  express();
+    createConnection().then((connection: Connection) => {
+      this.connection = connection;
+    }).catch(error => console.log(error));
+    this.app = express();
     this.config();
     this.routePrv.routes(this.app);
   }
